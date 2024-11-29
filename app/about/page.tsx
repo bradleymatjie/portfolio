0c44ee10aa0279d1e8ee0dd5255736b5508c7d2c
Bradley Matjie <77038935+bradleymatjie@users.noreply.github.com>
2024-11-30 00:19:00 +0200

    feat(up)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,97 @@
-import Construction from '../components/construction/page';
+import Construction from "../components/construction/page";
 
 export default function About() {
-    return (
-        <Construction />
-    )
+  return (
+    <section>
+      <div className="min-h-screen bg-light text-dark font-sans  flex flex-col justify-center items-center p-6">
+        {/* Main Section */}
+        <main
+          className="flex flex-col md:flex-row items-center justify-center  m-4"
+          style={{ height: "90vh", boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.1)" }}
+        >
+          {/* Profile Image */}
+          <div className="relative" style={{ width: "40%", height: "60%" }}>
+            <img
+              src="https://scontent-jnb2-1.xx.fbcdn.net/v/t39.30808-6/457094275_3299959966805512_701941671533872470_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=KVZGm317AiYQ7kNvgHQVSpB&_nc_zt=23&_nc_ht=scontent-jnb2-1.xx&_nc_gid=Aohqqa0HFnQeIp5qcKAWPDj&oh=00_AYBRGPZI2lcwKtpLVGVCyT3I8XJVc41EUGOh9Z0rT46QbA&oe=674FF7AE"
+              alt="Profile"
+              className="rounded-xl object-cover"
+              style={{ width: "100%", height: "100%" }}
+            />
+            <div className="absolute inset-0 rounded-xl bg-gradient-to-r from-red-500 to-black opacity-50"></div>
+          </div>
+
+          {/* Text Section */}
+          <div className="mt-6 md:mt-0 md:ml-12" style={{ width: "50%" }} >
+            <h1 className="text-5xl font-bold">Hi there!</h1>
+            <p className="mt-4 text-lg">
+            Driven by a passion for crafting innovative and user-friendly web experiences, I'm always eager to push boundaries, refine my skills, and deliver impactful solutions. Let’s dive into my journey below!
+            </p>
+            <button className="mt-6 px-6 py-3 bg-gray-800 hover:bg-gray-700 text-white rounded-md">
+              View my CV
+            </button>
+          </div>
+        </main>
+        <main 
+            className="flex flex-col md:flex-row p-8 " 
+            style={{ height: "60vh", boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.1)" }}
+        >
+          {/* Text Section */}
+          <div className="flex-1">
+            <h1 className="text-5xl font-bold mb-6">My Career Journey</h1>
+            <p className="text-md leading-relaxed mb-8">
+              Hi, I’m Bradley Matjie, a passionate Software Engineer with a deep
+              love for turning innovative ideas into reality. My journey in tech
+              began with a fascination for how software can transform lives, and
+              ever since, I've been on a mission to build seamless,
+              user-friendly digital experiences. With a solid foundation in
+              full-stack development from Melsoft Academy and extensive hands-on
+              experience with modern technologies like React, Next.js, and MERN
+              stack, I thrive on solving complex problems and delivering
+              scalable solutions. My expertise extends to API integrations,
+              creating dynamic, responsive interfaces, and optimizing
+              performance for web applications. What sets me apart is my
+              constant drive to learn and grow. Whether it's diving into new
+              frameworks like React Native or exploring Flutter, I’m always
+              ready to expand my skill set and push boundaries. Recently, I’ve
+              been refining my skills through Framer, WordPress, and even
+              exploring innovative 3D web design effects.
+            </p>
+            <p className="text-lg leading-relaxed">
+              When I'm not coding, you’ll find me exploring new technologies,
+              working on creative projects, or spending time with loved ones.
+            </p>
+          </div>
+
+          {/* Skills Section */}
+          <div className="flex-1 mt-8 md:mt-0 md:ml-12 pt-20">
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-4 text-light">
+              {[
+                "HTML5 & CSS3",
+                "JavaScript (ES6+)",
+                "React.js",
+                "Next.js",
+                "Responsive Web Design",
+                "API Integrations",
+                "Version Control (Git & GitHub)",
+                "Component-Based Architecture",
+                "Performance Optimization",
+                "SEO Best Practices",
+                "Testing & Debugging",
+                "RESTful APIs",
+                "Collaboration & Teamwork"
+              ].map((skill) => (
+                <span
+                  key={skill}
+                  className="px-4 py-2 bg-gray-800 rounded-full text-sm text-center text-light"
+                  style={{color:'aliceblue'}}
+                >
+                  {skill}
+                </span>
+              ))}
+            </div>
+          </div>
+        </main>
+      </div>
+    </section>
+  );
 }
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,7 +1,145 @@
-import Construction from "../components/construction/page";
+"use client";
+import "./portfolio.scss";
+import VigorCrossfit from "./vigor.png";
+import myBike from "./myBike.png";
+import { useRef, useState } from "react";
+import Cabinit from "./Cabinit.png";
+import fraktional from "./fraktional.png";
+import thooto from "./thooto.png";
 
 export default function Portfolio() {
-        return (
-            <Construction />
-    )
+    
+
+  const projects = [
+    {
+      id: 1,
+      name: "Vigor Crossfit",
+      url: "https://bradleymatjie.github.io/vigor_crossfit/",
+      description:
+        "A website for Vigor Crossfit, a gym dedicated to providing a supportive environment for individuals to achieve their fitness goals through CrossFit training.",
+      code: "https://github.com/bradleymatjie/vigor_crossfit",
+      image: `${VigorCrossfit.src}`,
+    },
+    {
+      id: 2,
+      name: "My Bike",
+      url: "https://bradleymatjie.github.io/",
+      description:
+        "The website presents a range of city bikes, highlighting features such as GPS tracking and performance specifications. It emphasizes models like 'Sporty 4' and 'Agile Ride 3' and offers test ride bookings.",
+      code: "https://github.com/bradleymatjie/bradleymatjie.github.io",
+      image: `${myBike.src}`,
+    },
+    {
+      id: 3,
+      name: "The Cabinit",
+      url: "https://bradleymatjie.github.io/",
+      description: "As a Frontend Engineer I worked on the Cabinit website, I was responsible for developing and maintaining the company's website and internal tools.",
+      code: "https://github.com/bradleymatjie/bradleymatjie.github.io",
+      image: `${Cabinit.src}`,
+    },
+    {
+        id: 4,
+        name: "Fraktional.dev",
+        url: "https://fraktional.dev",
+        description: "As a Frontend Engineer at Fraktional, I was responsible for developing and maintaining the company's website and internal tools. I focused on creating responsive, user-friendly layouts, optimizing performance, and collaborating with the team to integrate new features.",
+        code: "https://github.com/bradleymatjie/bradleymatjie.github.io",
+        image: `${fraktional.src}`,
+        },
+        {
+            id: 5,
+            name: "Thooto",
+            url: "https://thooto.com",
+            description: "As Lead Frontend Engineer at Param Solutions, I led the development of Thooto’s user interface. I focused on creating responsive, user-friendly layouts, optimizing performance, and collaborating with the team to integrate new features.",
+            code: "https://github.com/bradleymatjie/bradleymatjie.github.io",
+            image: `${thooto.src}`,
+        }
+  ];
+
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  function handleClick() {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ left: +300, behavior: 'smooth' });
+    }
+  }
+
+  function handleClickLeft() {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ left: -300, behavior: 'smooth' });
+    }
+  }
+
+  return (
+    <section className="flex items-center" style={{ height: "85vh" }}>
+      <div
+        className="flex justify-center items-end"
+        style={{ height: "100%", width: "30%" }}
+      >
+        <h1 className="text-7xl font-bold">Portfolio</h1>
+      </div>
+      <div
+        ref={scrollContainerRef}
+        className="flex items-center gap-10 overflow-hidden"
+        style={{ height: "100%", width: "70%" }}
+      >
+        <div className="flex flex-col gap-10 relative">
+        <button className="text-dark rounded-md p-2 nav-btn-left" onClick={handleClickLeft}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="40"
+              height="40"
+              fill="currentColor"
+              className="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
+              />
+            </svg>
+          </button>
+          <button className="text-dark rounded-md p-2 nav-btn" onClick={handleClick}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="40"
+              height="40"
+              fill="currentColor"
+              className="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
+              />
+            </svg>
+          </button>
+        </div>
+        {projects.reverse().map((project) => (
+          <div
+            className="card"
+            key={project.id}
+            // style={{ backgroundImage: `url(${project.image})` }}
+          >
+            <img src={project.image} alt={project.name} />
+            <h2 className="text-2xl font-bold">{project.name}</h2>
+            <p className="m-0">{project.description}</p>
+            <div className="buttons">
+              <button className="mt-6 px-6 py-3 bg-gray-800 hover:bg-gray-700 text-white rounded-md" onClick={() => window.open(project.url, '_blank')}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-fullscreen"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5M.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5m15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5" />
+                </svg>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
